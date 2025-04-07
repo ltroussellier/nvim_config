c7e39c03076dfef9e693c9c33a981e1c9373b83f
TROUSSELLIER Laurent <laurent.troussellier@ipsl.fr>
2025-04-07 16:49:18 +0200

    try neogit

--- lua/plugins/git-stuff.lua
+++ lua/plugins/git-stuff.lua
@@ -1,14 +1,67 @@
 return {
+  -- Load plenary first
+  { "nvim-lua/plenary.nvim", lazy = false },
+
+  -- Then diffview with explicit setup before neogit
   {
-    "tpope/vim-fugitive"
+    "sindrets/diffview.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      -- Make sure diffview is fully initialized before neogit uses it
+      local diffview = require("diffview")
+      diffview.setup({
+        enhanced_diff_hl = true,
+        view = {
+          default = {
+            layout = "diff2_horizontal",
+          },
+        },
+      })
+      vim.keymap.set("n", "<leader>gd", ":DiffviewOpen<CR>", {})
+      vim.keymap.set("n", "<leader>gq", ":DiffviewClose<CR>", {})
+    end,
+    lazy = false, -- Force this to load early
   },
+
+  -- Then neogit with careful integration setup
+  {
+    "TimUntersberger/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+    },
+    config = function()
+      -- Add a small delay to ensure diffview is fully loaded
+      vim.defer_fn(function()
+        require("neogit").setup({
+          integrations = {
+            diffview = true,
+          },
+          -- Disable auto diff to see if that helps
+          disable_context_highlighting = false,
+          disable_signs = false,
+          disable_hint = false,
+          disable_commit_confirmation = false,
+        })
+      end, 10)
+      vim.keymap.set("n", "<leader>gg", ":Neogit<CR>", {})
+    end,
+  },
+
+  -- Gitsigns (unchanged)
   {
     "lewis6991/gitsigns.nvim",
     config = function()
       require("gitsigns").setup()
-
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
-    end
-  }
+      local gs = require("gitsigns")
+      vim.keymap.set("n", "<leader>gp", gs.preview_hunk, {})
+      vim.keymap.set("n", "<leader>gt", gs.toggle_current_line_blame, {})
+      vim.keymap.set("n", "]h", gs.next_hunk)
+      vim.keymap.set("n", "[h", gs.prev_hunk)
+      vim.keymap.set("n", "<leader>gs", gs.stage_hunk)
+      vim.keymap.set("n", "<leader>gu", gs.undo_stage_hunk)
+      vim.keymap.set("n", "<leader>gr", gs.reset_hunk)
+      vim.keymap.set("n", "<leader>gl", gs.blame_line)
+    end,
+  },
 }
